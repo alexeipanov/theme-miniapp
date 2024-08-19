084d494b250c2c936cff863976298158ddc1b895
Alexei Panov <a8panov@gmail.com>
2024-08-19 19:57:10 +0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '/src/app.css';
+</script>
+
+<div class="wrapper">
+	<span class="title">Current theme colors</span>
+	<slot />
+</div>
+
+<style>
+	.wrapper {
+		height: calc(100vh - 2rem);
+		margin: 1rem;
+	}
+
+	.title {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { onMount } from 'svelte';
+	import { kebabCase } from 'lodash-es';
+	import { on } from '@telegram-apps/sdk';
+
+	let colors = [];
+
+	let themeColorNames = [
+		'accentTextColor',
+	  'bgColor',
+	  'buttonColor',
+	  'buttonTextColor',
+	  'destructiveTextColor',
+	  'headerBgColor',
+	  'hintColor',
+	  'linkColor',
+	  'secondaryBgColor',
+	  'sectionBgColor',
+	  'sectionHeaderTextColor',
+	  'subtitleTextColor',
+	  'textColor'
+	];
+
+	const getColors = (element) => {
+		return themeColorNames
+			.map((token) => getComputedStyle(element).getPropertyValue(`--tg-theme-${kebabCase(token)}`));
+	}
+
+	onMount(() => {
+		const element = document.querySelector('html');
+		colors = getColors(element);
+
+		on('theme_changed', () => {
+			const element = document.querySelector('html');
+	    colors = getColors(element);
+	  });
+	});
+
+	const copy = (color) => {
+		navigator.clipboard.writeText(color);
+	}
+</script>
+
+<div class="list">
+	{#each colors as color, index}
+		<div class="thumb" style="background-color: {color};"></div>
+		<!-- svelte-ignore a11y-interactive-supports-focus -->
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div>{themeColorNames[index]}
+			<br>
+			<div title="click to copy" role="button" class="color" on:click={copy(color)}>{color}</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.thumb {
+		width: 1.5rem;
+		height: 1.5rem;
+		border: 1px solid #eee;
+	}
+
+	.color {
+		font-family: monospace;
+		color: #999;
+	}
+</style>
